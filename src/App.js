6d05ace3b29d77e8f9a9cad904f2842ce60c4433
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-07-30 18:05:10 +0200

    instal react router v6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// const Router = require("react-router-dom").BrowserRouter;
+
+// const Routes = require("react-router-dom").Routes;
+
+// const Route = require("react-router-dom").Route;
+
 // import SignUp from "./components/SignUp/SignUp";
 // import LogIn from "./components/LogIn/LogIn";
-// import Home from "./components/Home/Home";
-// import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
-import Account from "./components/Account/Account";
+import Home from "./components/Home/Home";
+import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
+// import Account from "./components/Account/Account";
 import { GlobalStyle } from "./GlobalStyles";
 
-const App = () => {
-  return (
-    <>
-      <GlobalStyle></GlobalStyle>
-      <Account />
-    </>
-  );
-};
+const App = () => (
+  <Router>
+    <GlobalStyle></GlobalStyle>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="player" element={<MusicPlayer />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
