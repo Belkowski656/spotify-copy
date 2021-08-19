467df286f898f04f919b0664ca4146996dcdb2f0
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-08-19 22:01:12 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -29,13 +29,13 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("build"));
 
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     req.sendFile(path.resolve(__dirname, "build", "index.html"));
+//   });
+// }
 
 app.post("/delete-song-from-playlist", async (req, res) => {
   const { songId, playlistId } = req.body;
