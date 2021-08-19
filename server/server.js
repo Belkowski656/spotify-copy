ac547468b54c81913c88e90cc4430de0e85fd32f
Belkowski656 <hub.mezykprogramming@gmail.com>
2021-08-19 19:08:50 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,14 +15,11 @@ const Playlist = require("./models/playlist");
 const JWT_SECRET = "fanjasdfnjdsfin75454584858#@$@$!%dnfjdnf92ldsmkbfhud09";
 
 try {
-  mongoose.connect(
-    "mongodb+srv://belkowski:Microphone656!@cluster0.jcnbq.mongodb.net/spotify-copy?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }
-  );
+  mongoose.connect(`${process.env.MONGODB_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
 } catch (err) {
   console.log(err);
 }
